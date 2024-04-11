0d51449210eb712a186317460b6d41a378a8372a
allburov <allburov@gmail.com>
2024-04-11 13:26:18 +0700

    WAHA

--- ui/layouts/AppFooter.vue
+++ ui/layouts/AppFooter.vue
@@ -11,7 +11,7 @@ const logoUrl = computed(() => {
     <div class="layout-footer">
         <img :src="logoUrl" alt="Logo" height="20" class="mr-2" />
         by
-        <span class="font-medium ml-2">PrimeVue</span>
+        <span class="font-medium ml-2"><a href="https://devlike.pro/" targer="_blank">devlike.pro</a></span>
     </div>
 </template>
 <style lang="scss" scoped></style>
