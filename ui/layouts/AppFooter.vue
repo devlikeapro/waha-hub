bbc9b54f09f75be891f7694ea195ffafd5a1e6bd
allburov <allburov@gmail.com>
2024-04-11 13:51:36 +0700

    No footer

--- ui/layouts/AppFooter.vue
+++ ui/layouts/AppFooter.vue
@@ -8,10 +8,5 @@ const logoUrl = computed(() => {
 </script>
 
 <template>
-    <div class="layout-footer">
-        <img :src="logoUrl" alt="Logo" height="20" class="mr-2" />
-        by
-        <span class="font-medium ml-2"><a href="https://devlike.pro/" targer="_blank">devlike.pro</a></span>
-    </div>
 </template>
 <style lang="scss" scoped></style>
