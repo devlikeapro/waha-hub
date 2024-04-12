bd8d65159f21bf9d3bef4c9ebb14f40768010485
allburov <allburov@gmail.com>
2024-04-12 18:59:20 +0700

    Sessions

--- ui/components/ScreenshotButton.vue
+++ ui/components/ScreenshotButton.vue
@@ -1,4 +1,5 @@
 <script setup>
+const props  = defineProps(['disabled']);
 const op = ref(null);
 const toggle = (event) => {
   op.value.toggle(event);
@@ -7,7 +8,7 @@ const toggle = (event) => {
 </script>
 
 <template>
-  <Button type="button" icon="pi pi-camera" @click="toggle" rounded outlined/>
+  <Button :disabled="disabled" type="button" icon="pi pi-camera" @click="toggle" rounded outlined/>
   <OverlayPanel ref="op" appendTo="body" :showCloseIcon="true">
     <img src="/demo/images/nature/nature9.jpg" alt="Nature 9"/>
   </OverlayPanel>
