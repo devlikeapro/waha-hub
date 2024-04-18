dd8b49828f50f1eda54b105bf1394648b0c1e4aa
allburov <allburov@gmail.com>
2024-04-18 19:42:39 +0700

    Server dropdown

--- ui/service/inmemory/InMemoryRPCApi.ts
+++ ui/service/inmemory/InMemoryRPCApi.ts
@@ -4,30 +4,38 @@ import type {Session, SessionStatus} from "../Session";
 import {sleep} from "./utils";
 
 export class InMemoryRPCApi implements RPCApiClient {
-    call(serverId: ServerId, request: RPCRequest): Promise<any> {
+    private sessions = new Map<ServerId, Session[]>()
+
+    async call(serverId: ServerId, request: RPCRequest): Promise<any> {
+        const failed = serverId.endsWith("000");
+        if (failed) {
+            await sleep(3000)
+            throw new Error('Getting sessions failed');
+        }
+        // 0 - 1 sec delay
+        const timeout = Math.random() * 1000
+        await sleep(timeout)
         if (request.uri === '/api/sessions' && request.method === 'GET') {
             return this.getSessions(serverId);
         }
     }
 
     async getSessions(id: ServerId): Promise<Session[]> {
-        await sleep(1000)
-        const failed = id.endsWith("000");
-        if (failed) {
-            await sleep(3000)
-            throw new Error('Getting sessions failed');
+        const sessions = this.sessions.get(id)
+        if (sessions === undefined) {
+            const differentStatuses = id.endsWith("111");
+            const statuses: SessionStatus[] = differentStatuses ? ["WORKING", "FAILED", "SCAN_QR_CODE", "STARTING", "STOPPED"] : ["WORKING"]
+            const numbersOfSessions = 10
+            const newSessions = []
+            while (newSessions.length < numbersOfSessions) {
+                newSessions.push({
+                    name: `Session ${newSessions.length + 1}`,
+                    status: statuses[Math.floor(Math.random() * statuses.length)],
+                    config: {},
+                })
+            }
+            this.sessions.set(id, newSessions)
         }
-        const differentStatuses = id.endsWith("111");
-        const sessions: Session[] = []
-        const statuses: SessionStatus[] = differentStatuses ? ["WORKING", "FAILED", "SCAN_QR_CODE", "STARTING", "STOPPED"] : ["WORKING"]
-        const numbersOfSessions = 10
-        while (sessions.length < numbersOfSessions) {
-            sessions.push({
-                name: `Session ${sessions.length + 1}`,
-                status: statuses[Math.floor(Math.random() * statuses.length)],
-                config: {},
-            })
-        }
-        return sessions
+        return this.sessions.get(id)
     }
 }
