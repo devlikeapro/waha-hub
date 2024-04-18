094b06d0346065303f4f7002f5b461b53e20c24d
allburov <allburov@gmail.com>
2024-04-18 17:31:19 +0700

    Sessions

--- ui/components/SessionWebhook.vue
+++ ui/components/SessionWebhook.vue
@@ -1,11 +1,39 @@
 <script setup>
 const webhook = defineModel("webhook");
+const events = [
+  "session.status",
+  "message",
+  "message.any",
+  "message.reaction",
+  "message.ack",
+  "message.revoked",
+  "state.change",
+  "group.join",
+  "group.leave",
+  "presence.update",
+  "poll.vote",
+  "poll.vote.failed",
+]
 
 </script>
 
 <template>
-  <Fieldset legend="Header">
-    {{ webhook.url}}
+  <Fieldset :legend="webhook.url" toggleable>
+    <div class="field">
+      <label for="url">URL</label>
+      <InputText id="url" v-model.trim="webhook.url" required="true" autofocus/>
+    </div>
+
+    <div>
+      <label for="events">Events</label>
+      <MultiSelect
+          id="events"
+          v-model="webhook.events"
+          :options="events"
+          placeholder="Select Events"
+          class="w-25rem"
+      />
+    </div>
   </Fieldset>
 </template>
 
--- ui/components/SessionWebhooksField.vue
+++ ui/components/SessionWebhooksField.vue
@@ -2,27 +2,33 @@
 const webhooks = defineModel("webhooks");
 
 function add() {
-  webhooks.value.push({url: "http://"})
+  webhooks.value.push({
+    url: "https://",
+    events: ["session.status", "message", "message.reaction"],
+  })
 }
 </script>
 
 <template>
-  <div class="flex align-items-center">
-    <label>Webhooks</label>
-    <Button icon="pi pi-plus" text="" @click="add" severity="secondary"/>
-  </div>
+  <div class="flex flex-column gap-2">
+    <div>
+      <label>Webhooks</label>
+    </div>
+    <Button label="Add Webhook" icon="pi pi-plus" text="" @click="add" severity="secondary"/>
 
-  <template v-if="webhooks.length === 0">
-    <span class="text-300">
-      No webhooks configured
-    </span>
-  </template>
-  <template v-else>
-    <SessionWebhook
-        class="mb-2"
-        v-for="(webhook, index) in webhooks"
-        v-model:webhook="webhooks[index]"></SessionWebhook>
-  </template>
+    <template v-if="webhooks.length === 0">
+      <div class="text-300 text-center">
+        No webhooks configured
+      </div>
+    </template>
+    <template v-else>
+      <SessionWebhook
+          class="mb-2"
+          v-for="(webhook, index) in webhooks"
+          v-model:webhook="webhooks[index]"></SessionWebhook>
+    </template>
+
+  </div>
 </template>
 
 <style scoped lang="scss">
