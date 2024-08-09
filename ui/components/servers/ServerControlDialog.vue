bd54346e64c6b3c5e94ecd804b8fa99032fa2f10
allburov <allburov@gmail.com>
2024-08-09 19:34:39 +0700

    table

--- ui/components/servers/ServerControlDialog.vue
+++ ui/components/servers/ServerControlDialog.vue
@@ -1,10 +1,6 @@
 <script setup>
 const visible = defineModel("visible");
 const props = defineProps(['server'])
-const store = useServerStore()
-const isNewVersionAvailable = computed(() => {
-  return props.server.version?.version !== store.latestVersion
-})
 
 </script>
 
@@ -17,84 +13,7 @@ const isNewVersionAvailable = computed(() => {
       header='Server'
   >
     <h4>{{ server.name }}</h4>
-
-    <div class="p-3 flex flex-column gap-2">
-      <div class="flex gap-2">
-        <div>
-          Status:
-        </div>
-        <div class="flex flex-column gap-1">
-          <div>
-            <ServerConnectionIcon class="mr-1" :connected="server.connected"></ServerConnectionIcon>
-            <span> <b>
-            <template v-if="server.connected">
-            Connected
-            </template>
-            <template v-else>
-            Disconnected
-            </template>
-          </b> </span>
-          </div>
-        </div>
-      </div>
-
-      <div class="flex gap-1">
-        <div>
-          API URL:
-        </div>
-        <div>
-          <a :href="server.connection?.url" target="_blank" class="ml-2">{{ server.connection?.url }}
-            <i class="pi pi-external-link"></i>
-          </a>
-        </div>
-      </div>
-
-      <div class="flex gap-2">
-        <div>
-          Engine:
-        </div>
-        <div>
-          <code>
-            {{ server.version?.engine }}
-          </code>
-        </div>
-      </div>
-
-      <div class="flex gap-2">
-        <div>
-          Version:
-        </div>
-        <div>
-          <code
-              :class="{
-            'text-orange-400': isNewVersionAvailable,
-          }"
-          >
-            {{ server.version?.version }}
-          </code>
-          <template v-if="!isNewVersionAvailable">
-            <span class="text-green-500 font-medium ml-2">
-              <i class="pi text-green-500 pi-check-circle"></i>
-              Up to date
-            </span>
-          </template>
-        </div>
-      </div>
-
-      <InlineMessage
-          v-if="isNewVersionAvailable"
-          severity="info">
-        There's a new
-        <span class="text-900 font-medium">
-              <a href="https://waha.devlike.pro/docs/overview/changelog/" target="_blank">
-                {{ store.latestVersion }}
-                <i class="pi pi-external-link"></i>
-              </a>
-              </span>
-        version available!
-      </InlineMessage>
-    </div>
-
+    <ServerStatus :server="props.server"></ServerStatus>
 
     <h4>Variables</h4>
     <ServerVariablesTable :server="props.server"></ServerVariablesTable>
