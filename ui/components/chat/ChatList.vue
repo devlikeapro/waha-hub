d44e6195d6c5bb9ef0939d0f7879197388862aa4
devlikepro <waha@devlike.pro>
2026-04-14 11:56:32 +0700

    SHow More fix

--- ui/components/chat/ChatList.vue
+++ ui/components/chat/ChatList.vue
@@ -18,63 +18,61 @@ const emit = defineEmits(['clickOnChat',
 </script>
 
 <template>
-  <DataTable
-      :value="chats"
-      :loading="pending"
-      style="width: 100%"
-      class="p-datatable--clickable"
-      @row-click="emit('clickOnChat', $event.data)"
-      scrollable
-      scrollHeight="100%"
-      :rowHover="true"
-  >
-    <Column>
-      <template #header>
-        <div class="w-full flex align-items-center gap-3 flex-wrap justify-content-between">
-          <div class="flex align-items-center gap-2">
-            <div>
-              {{ $t('chat.last20Chats') }}
+  <div style="width: 100%; height: 100%; overflow: auto; display: flex; flex-direction: column;">
+    <DataTable
+        :value="chats"
+        :loading="pending"
+        style="width: 100%"
+        class="p-datatable--clickable"
+        @row-click="emit('clickOnChat', $event.data)"
+        :rowHover="true"
+    >
+      <Column>
+        <template #header>
+          <div class="w-full flex align-items-center gap-3 flex-wrap justify-content-between">
+            <div class="flex align-items-center gap-2">
+              <div>
+                {{ $t('chat.last20Chats') }}
+              </div>
+              <RefreshButton
+                  @click="emit('refreshChats')"
+                  :refreshing="pending"
+              ></RefreshButton>
             </div>
-            <RefreshButton
-                @click="emit('refreshChats')"
-                :refreshing="pending"
-            ></RefreshButton>
+            <div
+                class="flex align-items-center gap-2 ml-auto"
+                v-tooltip.bottom="$t('chat.mergeToggleTooltip')"
+            >
+              <i class="pi pi-object-ungroup text-sm"></i>
+              <span class="font-medium text-sm">{{ $t('chat.mergeToggleLabel') }}</span>
+              <InputSwitch
+                  :modelValue="props.merge"
+                  @update:modelValue="value => emit('update:merge', value)"
+              />
+            </div>
           </div>
-          <div
-              class="flex align-items-center gap-2 ml-auto"
-              v-tooltip.bottom="$t('chat.mergeToggleTooltip')"
-          >
-            <i class="pi pi-object-ungroup text-sm"></i>
-            <span class="font-medium text-sm">{{ $t('chat.mergeToggleLabel') }}</span>
-            <InputSwitch
-                :modelValue="props.merge"
-                @update:modelValue="value => emit('update:merge', value)"
-            />
-          </div>
-        </div>
-      </template>
-      <template #body="{ data }">
-        <ChatPreview
-            :id="data.id"
-            :name="data.name"
-            :picture="data.picture"
-            :message="data.lastMessage"
-        />
-      </template>
-      <template #footer>
-        <div class="flex justify-content-center py-2">
-          <Button
-              :label="$t('chat.showMoreChats')"
-              icon="pi pi-arrow-down"
-              size="small"
-              outlined
-              :loading="loadingMoreChats"
-              @click="loadMoreChats"
+        </template>
+        <template #body="{ data }">
+          <ChatPreview
+              :id="data.id"
+              :name="data.name"
+              :picture="data.picture"
+              :message="data.lastMessage"
           />
-        </div>
-      </template>
-    </Column>
-  </DataTable>
+        </template>
+      </Column>
+    </DataTable>
+    <div class="flex justify-content-center py-5">
+      <Button
+          :label="$t('chat.showMoreChats')"
+          icon="pi pi-arrow-down"
+          size="small"
+          outlined
+          :loading="loadingMoreChats"
+          @click="loadMoreChats"
+      />
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
